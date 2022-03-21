5ea1f7b29edd512ad349f9eddf11c00d944daf25
naiden <na4ko359@gmail.com>
2022-03-21 17:36:42 +0200

    style improvements

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function grid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < (rows * cols); i++) {
-      let cell = document.createElement("div");
-      container.appendChild(cell).className = "grid-item";
-      cell.addEventListener("mouseenter", e => {
-          let randomNumber1 = Math.floor(Math.random() * 256);
-          let randomNumber2 = Math.floor(Math.random() * 256);
-          let randomNumber3 = Math.floor(Math.random() * 256);
-
-          e.target.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3}`;
-      })
-    };
+function grid(rows) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', rows);
+  for (i = 0; i < (rows * rows); i++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+    cell.addEventListener("mouseenter", e => {
+        let randomNumber1 = Math.floor(Math.random() * 256);
+        let randomNumber2 = Math.floor(Math.random() * 256);
+        let randomNumber3 = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3}`;
+    });
   };
-  
-  grid(32, 32);
+};
+grid(16);
+
