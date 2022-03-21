58abbb2d897acd38c58547b2039f9ce8198ee862
naiden <na4ko359@gmail.com>
2022-03-21 08:05:49 +0200

    random color code refactor

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let color = ["#f5f972", "#6089cf", "#49ee97", "#c3e085", "#792af9", "#567e9f",
-"#e53ba2", "#4f6aa7", "#fe4910", "#13f3cd", "#438331", "#31d086", "#c359f8", "#3b45a9",
-"#7fd2bc", "#a10a2d", "#7c4940", "#36f0c9", "#415543", "#975da5", "#138fb9"];
-
 function grid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -9,9 +5,13 @@ function grid(rows, cols) {
       let cell = document.createElement("div");
       container.appendChild(cell).className = "grid-item";
       cell.addEventListener("mouseenter", e => {
-          e.target.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
+          let randomNumber1 = Math.floor(Math.random() * 256);
+          let randomNumber2 = Math.floor(Math.random() * 256);
+          let randomNumber3 = Math.floor(Math.random() * 256);
+
+          e.target.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3}`;
       })
     };
   };
   
-  grid(64, 100);
+  grid(32, 32);
